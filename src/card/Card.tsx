fc9063ae03c828cf0a402fa30937069f9df63834
AntonovAtanas <atanas.antonov1@gmail.com>
2023-10-25 11:37:56 +0300

    Add click event handler to show/hide the answer

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,17 +1,31 @@
-import questions from '../cards-db';
-import './Card.css';
+import { useState } from "react";
+import questions from "../cards-db";
+import "./Card.css";
 
 const Card = () => {
+  const [clickedCardId, setclickedCardId] = useState<number | null>(null);
 
-    return (
-        <div className="flashcards">
-                {questions.map(question => (
-                    <div key={question.id}>
-                        <p>{question.question}</p>
-                    </div>
-                ))}
+  function onSelectedCard(cardId: number | null) {
+    if (clickedCardId !== cardId || cardId === null) {
+      setclickedCardId(cardId);
+    } else {
+      setclickedCardId(null);
+    }
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => onSelectedCard(question.id)}
+          className={clickedCardId === question.id ? "selected" : ""}
+        >
+          <p>{clickedCardId === question.id ? question.answer : question.question }</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Card;
